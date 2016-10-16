946c6fbdf6b4513e0a1e9e04c1956dfa01158d13
Serega <serv25@i.ua>
2016-10-16 21:59:19 +0300

    up

--- app/src/main/java/com/example/serega/pocketnews/AddNewsActivity.java
+++ app/src/main/java/com/example/serega/pocketnews/AddNewsActivity.java
@@ -1,31 +1,96 @@
 package com.example.serega.pocketnews;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AddNewsActivity extends Activity {
+
+    private static String photoPath;
+    private String title;
+    private String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_news);
 
-        Button addNewsBtn = (Button) findViewById(R.id.add_news);
-        addNewsBtn.setOnClickListener(onClickAddButton);
+        Button btnPublish = (Button) findViewById(R.id.publish_btn);
+        btnPublish.setOnClickListener(onClickPublishBtn);
+
+        Button btnTakePhoto = (Button) findViewById(R.id.take_photo);
+        btnTakePhoto.setOnClickListener(onClickTakePhotoBtn);
+
     }
 
-    private View.OnClickListener onClickAddButton = new View.OnClickListener() {
+    private View.OnClickListener onClickPublishBtn = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             EditText addedTitle = (EditText) findViewById(R.id.add_title);
-            String title = addedTitle.getText().toString();
+            title = addedTitle.getText().toString();
 
             EditText addedText = (EditText) findViewById(R.id.add_text);
-            String text = addedText.getText().toString();
+            text = addedText.getText().toString();
+
+            News news = new News(title, photoPath, text);
+            news.save();
 
         }
     };
+
+    private View.OnClickListener onClickTakePhotoBtn = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            takePicture();
+            if (title != null && text != null) {
+                startActivity(new Intent(AddNewsActivity.this, MainActivity.class));
+            }else{
+                Toast.makeText(AddNewsActivity.this, "Please fill out all fields!", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    };
+
+
+    public void takePicture() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri file = Uri.fromFile(getOutputMediaFile());
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+        startActivityForResult(intent, 100);
+    }
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        photoPath = mediaStorageDir.getPath() + File.separator + "IMG_" + timeStamp + ".jpg";
+        return new File(photoPath);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 100) {
+            if (resultCode == RESULT_OK) {
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/serega/pocketnews/MainActivity.java
+++ app/src/main/java/com/example/serega/pocketnews/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.serega.pocketnews;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridLayout grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        grid = (GridLayout) findViewById(R.id.grid_layout);
+        showNews();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showNews();
     }
 
     @Override
@@ -19,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_items, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -27,5 +49,44 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showNews() {
+        grid.removeAllViews();
+
+        List<News> list = News.listAll(News.class);
+        LinearLayout linearLayout;
+        ImageView photoView;
+        TextView titleView;
+        TextView textView;
+
+        if (list != null) {
+            for (News news : list) {
+                linearLayout = new LinearLayout(this);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+                photoView = new ImageView(this);
+                photoView.setImageBitmap(getDrawableFromStr(news.getPhoto()));
+
+                titleView = new TextView(this);
+                titleView.setText(news.getTitle());
+
+                textView = new TextView(this);
+                textView.setText(news.getText());
+
+                linearLayout.addView(photoView);
+                linearLayout.addView(titleView);
+                linearLayout.addView(textView);
+
+                grid.addView(linearLayout);
+            }
+        }
+
+    }
+
+    private Bitmap getDrawableFromStr(String path) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 8;
+        return BitmapFactory.decodeFile(path, options);
     }
 }
